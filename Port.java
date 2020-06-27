c6a0c05a5c1f56901bede7ca3b9702db456e5902
Oatmeal107 <66317476+Oatmeal107@users.noreply.github.com>
2020-06-27 09:46:50 +0800

    更新代码

---
+++ Port.java
@@ -0,0 +1,18 @@
+
+package Model;
+
+public interface Port {
+	/**
+	 * 
+	 * 模型更新
+	 * 
+	 */
+	public abstract void updata(long tick);
+	
+	/**
+	 * 
+	 * 游戏初始化
+	 * 
+	 */
+	public abstract void startGameInit();
+}
