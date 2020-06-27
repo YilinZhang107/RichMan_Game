c610489d65ae24c74a0235ccc61077813760c723
Oatmeal107 <66317476+Oatmeal107@users.noreply.github.com>
2020-06-27 10:15:31 +0800

    更新代码

---
+++ src/model/cards/CrossingCard.java
@@ -0,0 +1,28 @@
+package Model.cards;
+
+
+import context.GameState;
+import Model.PlayerModel;
+
+/**
+ * 
+ * 
+ *嫁祸卡,遇到危险时生效，嫁祸他人大量租金
+ * 
+ *
+ */
+public class CrossingCard extends Card{
+
+	public CrossingCard(PlayerModel owner) {
+		super(owner);
+		this.name = "CrossingCard";
+		this.cName = "嫁祸卡";
+		this.price = 120;
+	}
+
+	@Override
+	public int useCard() {
+		return GameState.CARD_CROSSING;
+	}
+
+}
